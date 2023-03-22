f0713545c37a6e5116b020099a135256d61d899b
Juliane Fiates <102069987+JFiates@users.noreply.github.com>
2023-03-22 20:38:36 -0300

    Update cudacompile.sh

--- cudacompile.sh
+++ cudacompile.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-nvcc -O3 -prec-div false -prec-sqrt false -fmad false -Xcompiler -fopenmp -std=c++11 -gencode arch=compute_61,code=sm_61  -M -o "viscosityv2-cuda.d" "viscosityv2-cuda.cu"
+nvcc -O3 -prec-div false -prec-sqrt false -fmad false -Xcompiler -fopenmp -std=c++11 -gencode arch=compute_61,code=sm_61  -M -o "viscosityvf-cuda.d" "viscosityvf-cuda.cu"
 
-nvcc -O3 -prec-div false -prec-sqrt false -fmad false -Xcompiler -fopenmp -std=c++11 --compile --relocatable-device-code=false -gencode arch=compute_61,code=compute_61 -gencode arch=compute_35,code=sm_35  -x cu -o  "viscosityv2-cuda.o" "viscosityv2-cuda.cu"
+nvcc -O3 -prec-div false -prec-sqrt false -fmad false -Xcompiler -fopenmp -std=c++11 --compile --relocatable-device-code=false -gencode arch=compute_61,code=compute_61 -gencode arch=compute_35,code=sm_35  -x cu -o  "viscosityvf-cuda.o" "viscosityvf-cuda.cu"
 
-nvcc --cudart static --relocatable-device-code=false -gencode arch=compute_35,code=compute_35 -gencode arch=compute_61,code=sm_61 -link -o  "viscosity-cuda"  viscosityv2-cuda.o   -lgomp
+nvcc --cudart static --relocatable-device-code=false -gencode arch=compute_35,code=compute_35 -gencode arch=compute_61,code=sm_61 -link -o  "viscosity-cuda"  viscosityvf-cuda.o   -lgomp
 
